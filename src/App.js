b7a651fcefb62014d458ba3f62113b5701d6580d
rihab <rihab@1D-RIHABB.DC-CAPES.COM>
2021-05-24 23:29:40 +0100

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
+import imageInSrc from './imageInSrc.jpg';
 import './App.css';
+import "./styles.css";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div style={{border:"solid 1px black",maxWidth:"100vw"}}>
+     
+
+<h1 class="title red">Your name here</h1>
+<br/>
+<img className="image" src={imageInSrc} alt="imageInSrc" />
+
+<br/>
+
+
+
+<img className="image" src="/imageInPublic.jpg" alt="imagePublic" />
+
+    
     </div>
+    <video width="320" height="240" controls>
+
+    <source src="/myVideo.mp4" type="video/mp4" />
+   
+   </video>
+   </>
   );
 }
 
